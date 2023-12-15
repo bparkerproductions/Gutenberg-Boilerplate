12970a834629a805f37b1e21949ff466ecfa2c31
Brandon Parker <bparkerproductions@gmail.com>
2023-12-14 19:42:23 -0800

    remove readme and build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a6646b404a3d751c2f02');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '0a32fc6becf6a6736e28');
